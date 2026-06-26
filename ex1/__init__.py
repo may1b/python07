a8455bda723a7aceec8c7a255d6bac1f7c3d2a41
May <magrass@student.42heilbronn.de>
2026-06-26 23:45:07 +0200

    fixed small thing in ex1

--- ex1/__init__.py
+++ ex1/__init__.py
@@ -1,14 +1,6 @@
-from ex0.factory import CreatureFactory
-from ex1.capability import HealCapability, TransformCapability
-from ex1.creature import HealingCreature, TransformingCreature
 from ex1.factory import HealingCreatureFactory, TransformCreatureFactory
 
 __all__: list[str] = [
-    "CreatureFactory",
-    "HealCapability",
-    "TransformCapability",
-    "HealingCreature",
-    "TransformingCreature",
     "HealingCreatureFactory",
     "TransformCreatureFactory",
 ]
